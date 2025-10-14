c2353f143dbc5587d0ffd4f9b017ad720cbdf76c
MarioGitLaser <82460429+MarioGitLaser@users.noreply.github.com>
2025-10-14 17:12:43 -0600

    primer commit

--- Db/consulta reporte operador.sql
+++
@@ -1,8 +0,0 @@
-SELECT count(*) as cotizados, sum(Servicio_realizado) as confirmados, CAST(sum(Servicio_realizado) as REAL)/count(*)*100 as porcentaje, b.Nombre
-FROM Cotizacion  a INNER JOIN Operador b on a.Id_Operador = b.Id
-WHERE DATE(Fecha) BETWEEN '2021-09-20' AND '2021-09-22'
-GROUP by Id_Operador
-
-SELECT b. Nombre, Cliente from Cotizacion a INNER JOIN Operador b on a.Id_Operador = b.Id
-
-INSERT INTO Operador (Nombre) VALUES ('Adrian')
